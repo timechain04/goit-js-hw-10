e3be60029b06769b2e2e7366e64dd3ad44f4fd62
Andrii Lutsiak <timechain04@gmail.com>
2023-06-21 17:00:23 +0300

    api

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,17 +1,21 @@
 const BASE_URL = 'https://api.thecatapi.com/v1';
 const API_KEY = 'live_tzTsyw7krgEZLBeoJMmi7g8WFICFdg0xoTjCAS3CjqcXEkmhgwSfsWDDoUqLh3OS';
-const SEARCH_CAT = `${BASE_URL}/images/search?limit=1&breed_ids=`;
-const searchOptions = { headers: { 'x-api-key': API_KEY },};
 
-export const fetchBreeds = function() {
-  console.info('start fetch')
-  return fetch(`${BASE_URL}/breeds`)
-  .then(response => response.json())
+export function fetchBreeds() {
+  return fetch(`${BASE_URL}/breeds/?api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText || response.status);
+    }
+
+    return response.json();
+  });
 }
 
-export const fetchCatByBreed = function(breedId) {
-return fetch(`${SEARCH_CAT}${breedId}`,searchOptions)
-.then(response => {return response.json()})
+export function fetchCatByBreed(id) {
+  return fetch(`${BASE_URL}/images/search?breed_ids=${id}&api_key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText || response.status);
+    }
+    return response.json();
+  });
 }
-
-
